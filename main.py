546441de1e794445fb9429657abc7df6008653f4
Ryan Monteiro <ryanmonteiro@users.noreply.github.com>
2023-06-27 21:52:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ LIMITE_QNT_SAQUES = 3
 hist_transacoes = []
 
 
-def depositar():
-    global saldo
-    valor = float(input("Digite o valor que deseja depositar:"))
+def depositar(saldo, valor, hist_transacoes, /):
     if valor > 0:
         saldo += valor
         hist_transacoes.append(valor)
@@ -29,36 +27,38 @@ def depositar():
         print("Não é possível depositar um valor 0 ou negativo.")
         print("Por favor, repita a operação corretamente")
 
+    return saldo
 
-def sacar(aux_saldo, aux_valor_saque, aux_hist_transacoes, aux_limite_vlr_saque, aux_numero_saques, aux_limite_saques):
-    if (aux_valor_saque > 0) and (aux_valor_saque < aux_limite_vlr_saque) and (aux_numero_saques < aux_limite_saques):
-        if aux_valor_saque <= aux_saldo:
-            aux_saldo -= aux_valor_saque
-            aux_valor_saque *= -1
-            aux_hist_transacoes.append(aux_valor_saque)
-            aux_numero_saques += 1
+
+def sacar(*, saldo, valor_saque, hist_transacoes, limite_vlr_saque, numero_saques, limite_saques):
+    if (valor_saque > 0) and (valor_saque <= limite_vlr_saque) and (numero_saques < limite_saques):
+        if valor_saque <= saldo:
+            saldo -= valor_saque
+            valor_saque *= -1
+            hist_transacoes.append(valor_saque)
+            numero_saques += 1
             print("Saque efetuado com sucesso.")
-            print("Saldo atual: R$", aux_saldo)
+            print("Saldo atual: R$", saldo)
         else:
             print("Saldo insuficiente para realizar o saque.")
-            print("Saldo atual: R$", aux_saldo)
+            print("Saldo atual: R$", saldo)
 
-    elif aux_valor_saque > aux_limite_vlr_saque:
+    elif valor_saque > limite_vlr_saque:
         print("Valor desejado maior do que o limite diário de R$ 500.00")
         print("Por favor, repita a operação corretamente")
 
-    elif aux_numero_saques >= aux_limite_saques:
+    elif numero_saques >= limite_saques:
         print("Limite de saques diários atingidos")
         print("Por favor, repita a operação no próximo dia útil")
 
     else:
         print("Não é possível sacar um valor 0 ou negativo.")
         print("Por favor, repita a operação corretamente")
-    return aux_saldo, aux_hist_transacoes, aux_numero_saques
+
+    return saldo, numero_saques
 
 
-def extrato():
-    global hist_transacoes, saldo
+def extrato(saldo, /, *, hist_transacoes):
     print("==================Extrato==================")
     for i in range(0, len(hist_transacoes)):
         format_out = "{:.2f}".format(hist_transacoes[i])
@@ -75,16 +75,17 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        depositar()
+        valor_deposito = float(input("Digite o valor que deseja depositar:"))
+        saldo = depositar(saldo, valor_deposito, hist_transacoes)
 
     elif opcao == "s":
         valor_saque = float(input("digite o valor que deseja sacar:"))
-        sacar(aux_saldo=saldo, aux_valor_saque=valor_saque,
-              aux_hist_transacoes=hist_transacoes, aux_limite_vlr_saque=limite_vlr_saque,
-              aux_numero_saques=numero_saques, aux_limite_saques=LIMITE_QNT_SAQUES)
+        saldo, numero_saques = sacar(saldo=saldo, valor_saque=valor_saque,
+                                     hist_transacoes=hist_transacoes, limite_vlr_saque=limite_vlr_saque,
+                                     numero_saques=numero_saques, limite_saques=LIMITE_QNT_SAQUES)
 
     elif opcao == "e":
-        extrato()
+        extrato(saldo, hist_transacoes=hist_transacoes)
 
     elif opcao == "q":
         print("Obrigado por utilizar o Yoshi's Bank!")
